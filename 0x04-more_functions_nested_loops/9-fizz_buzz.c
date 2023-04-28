ca45018c0fcefe722cd23d2f94f615e49d29f00b
Regomoditsoe <rsmramarumo@gmail.com>
2023-04-28 16:38:48 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
-printf("Fizzbuzz");
+printf("FizzBuzz");
 else if (i % 3 == 0)
 printf("Fizz");
 else if (i % 5 == 0)
@@ -27,6 +27,6 @@ printf("%i", i);
 if (i < 100)
 printf(" ");
 }
-printf('\n');
+printf("\n");
 return (0);
 }
