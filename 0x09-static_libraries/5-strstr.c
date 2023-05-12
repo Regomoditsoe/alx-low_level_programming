4578ca611e178b638072871fb464bb5f4f4d1819
Regomoditsoe <rsmramarumo@gmail.com>
2023-05-12 13:14:28 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -10,22 +10,25 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int index;
+	int index;
+	
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+	index = 0;
 
-if (*needle == 0)
-return (haystack);
-while (*haystack)
-{
-index = 0;
-if (haystack[index] == needle[index])
-{
-do {
-if (needle[index + 1] == '\0')
-return (haystack);
-index++;
-} while (haystack[index] == needle[index]);
-}
-haystack++;
-return ('\0');
-}
+	if (haystack[index] == needle[index])
+	{
+		do {
+			if (needle[index + 1] == '\0')
+			return (haystack);
+			index++;
+
+			} while (haystack[index] == needle[index]);
+	}
+	haystack++;
+
+	return ('\0');
+	}
 }
