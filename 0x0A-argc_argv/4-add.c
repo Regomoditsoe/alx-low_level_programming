037b6728479401dcdfcf24065cd34fd511c7efa6
Regomoditsoe <rsmramarumo@gmail.com>
2023-05-13 11:37:08 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check if string consists of digits
+ * @str: input string
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int check_num(char *str)
+{
+	unsigned int count = 0;
+
+	while (count < strlen(str))
+	{
+	if (!isdigit(str[count]))
+	{
+		return (0);
+	}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: number of arguments
+ * @argv: array of argument strings
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+	if (check_num(argv[count]))
+	{
+	str_to_int = atoi(argv[count]);
+	sum += str_to_int;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
